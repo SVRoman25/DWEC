64b6be139dd5776c72c82f3b81ba15d7df5798d5
SVRoman25 <samuelvidalroman@gmail.com>
2020-09-23 10:05:51 +0200

    PracticaJS 23/sept/2020

--- PracticaJS/scripts.js
+++ PracticaJS/scripts.js
@@ -1,44 +1,77 @@
 
     const button = document.getElementById('box');
     const tecla  = document.getElementById("textito");
+    const text = document.getElementById("frase");
+    const envi = document.getElementById("enviar");
+    const galeria=document.getElementById("gallery");
+
+    //Ejercicio 1
     button.addEventListener('mouseenter', () => {
         
         button.style.backgroundColor="green";
     
-    })
+    });
 
     button.addEventListener('mouseleave', () => {
         
         button.style.backgroundColor="red";
     
-    })
+    });
 
     button.addEventListener('mousedown', () => {
     
         console.log("Has pulsado la caja");
     
-    })
+    });
 
     button.addEventListener('mouseup', () => {
         
         console.log("Has soltado el boton izquierdo dentro de la caja");
       
-      })
+      });
 
-      tecla.addEventListener("keydown" ,() =>{
 
-        console.log("Has pulsado una tecla");
+
+    //Ejercicio 2
+    tecla.addEventListener("keydown" ,() =>{
+
+        onsole.log("Has pulsado una tecla");
+
+    });
+
+    tecla.addEventListener("keyup" ,() =>{
+
+      console.log("Has soltado una tecla");
+
+    });
+
+    tecla.addEventListener("keydown" ,(e) =>{
+
+      console.log(e.key); //Con key podemos saber que tecla pulsamos
+
+    });
+
+    //Ejercicio 3
+    envi.addEventListener("click", (d)=>{
+      d.preventDefault();
+
+      text.addEventListener("keyup" ,(e) =>{
+
+        console.log(e.key);
 
       });
 
-      tecla.addEventListener("keyup" ,() =>{
+    });
 
-        console.log("Has soltado una tecla");
 
-      });
+    //Ejercicio 4
+    galeria.addEventListener("click", (e)=>{
+      e.preventDefault();
+      
+      console.log(e.target.textContent);
+    });
 
-      tecla.addEventListener("keydown" ,(e) =>{
 
-        console.log(e.key); //Con key podemos saber que tecla pulsamos
+    
 
-      });
+    
